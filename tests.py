bb2b8d0f31cd6865b0378747e37aa9ea0fbdb348
krzysztofsmokowski <krzysztofsmokowski@gmail.com>
2018-05-10 21:12:21 +0200

    added requirements, now trying to get tests on with magicmock

--- tests.py
+++ tests.py
@@ -1,10 +1,15 @@
 from shorter import html_paragraphs
 import unittest
+from mock import MagicMock
 
 class ShorterModuleTest(unittest.TestCase):
     def test_html_paragraphs(self):
-        soup = 'somethingsomething'
-        self.assertEqual(soup, html_paragraphs('https://www.crummy.com/software/BeautifulSoup/bs4/doc/'))
+        soup = MagicMock(return_value=[
+            "paragraph1"
+            "paragraph2"
+            ])
+        function_test = html_paragraphs('https://www.crummy.com/software/BeautifulSoup/bs4/doc/')
+        self.assertEqual(soup, function_test)
 
 if __name__ == '__main__':
     unittest.main()
