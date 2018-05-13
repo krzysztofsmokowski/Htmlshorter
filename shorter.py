69db26d507794a7b1c3ea439f56aff7fbd4c3a7c
krzysztofsmokowski <krzysztofsmokowski@gmail.com>
2018-05-13 10:24:32 +0200

    fixed return problem and request usage in tests

--- shorter.py
+++ shorter.py
@@ -4,12 +4,8 @@ import requests
 def html_paragraphs(url):
     requested_page = requests.get('{}'.format(url))
     soup = BeautifulSoup(requested_page.text, 'html.parser')
+    paragraphs = ''
     for paragraph_element in soup.find_all('p'):
-        print(paragraph_element.text)
+        paragraphs += paragraph_element.text
+    return paragraphs
 
-def main():
-    html_paragraphs('https://www.crummy.com/software/BeautifulSoup/bs4/doc/')
-
-
-if __name__ == '__main__':
-    main()
--- tests.py
+++ tests.py
@@ -4,12 +4,8 @@ from mock import MagicMock
 
 class ShorterModuleTest(unittest.TestCase):
     def test_html_paragraphs(self):
-        soup = MagicMock(return_value=[
-            "paragraph1"
-            "paragraph2"
-            ])
-        function_test = html_paragraphs('https://www.crummy.com/software/BeautifulSoup/bs4/doc/')
-        self.assertEqual(soup, function_test)
+        html_paragraphs = MagicMock(return_value=["paragraph1 paragraph2"])
+        self.assertEqual("paragraph1 paragraph2", html_paragraphs)
 
 if __name__ == '__main__':
     unittest.main()
